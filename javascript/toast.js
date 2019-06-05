04229c5f035ae588002e8cf804f5325a3c927e21
rishanshah <rishanshah@hotmail.co.uk>
2019-06-05 16:08:09 +0100

    edit to testimonials

--- javascript/toast.js
+++ javascript/toast.js
@@ -1,6 +1,7 @@
 function myFunction() {
   var x = document.getElementById("warning toast");
+  x.style.transition= "1.5s"
   x.style.height = 0;
   x.style.display = "None";
-  x.style.transition= "1.5s";
+  ;
 }
